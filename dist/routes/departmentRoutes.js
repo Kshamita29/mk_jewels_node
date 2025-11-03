0aedcdc955d8b4b147e269cf8cb711c9f0ccafb2
Kshamita Kiran Baviskar <kshamita2920@gmail.com>
2025-11-03 13:51:29 +0000

    build changed to echo

--- dist/routes/departmentRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getDepartments, addDepartment, updateDepartment, deleteDepartment } from './controllers/departmentController.js';
-const router = express.Router();
-router.get('/', getDepartments);
-router.post('/', addDepartment);
-router.put('/:id', updateDepartment);
-router.delete('/:id', deleteDepartment);
-export default router;
--- dist/routes/staffRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getStaff, addStaff, updateStaff, deleteStaff } from './controllers/staffController.js';
-const router = express.Router();
-router.get('/', getStaff);
-router.post('/', addStaff);
-router.put('/:id', updateStaff);
-router.delete('/:id', deleteStaff);
-export default router;
